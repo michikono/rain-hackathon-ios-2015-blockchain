b7ca72a8cc2f254d9efe57bbea6d24c8c20164bb
Renu <rpunjabi12@gmail.com>
2015-07-11 01:30:04 -0700

    Added DetailVC class and storyboard + added navigation bar

---
+++ AutoBit/DetailVC.swift
@@ -0,0 +1,14 @@
+//
+//  DetailVC.swift
+//  AutoBit
+//
+//  Created by Renu Punjabi on 7/11/15.
+//  Copyright (c) 2015 Eric Cerney. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailVC: UIViewController {
+    
+}
